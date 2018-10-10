34c22adfe2450c6dc27e9f81c6c411ca9f196d82
DenisLobur <denisdenisdenis@bigmir.net>
2018-10-10 15:03:28 +0300

    709. ToLowerCase

---
+++ src/main/scala/easy/ToLowerCase.scala
@@ -0,0 +1,14 @@
+package easy
+
+/**
+  * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+  *
+  * Example1
+  * Input: "Hello"
+  * Output: "hello"
+  */
+object ToLowerCase {
+  def toLowerCase(str: String): String = {
+    str.toLowerCase
+  }
+}
